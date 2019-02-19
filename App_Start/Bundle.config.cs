edd80613711f56465a9566780e84ed021255ae21
pulipatisatish2012 <36352775+pulipatisatish2012@users.noreply.github.com>
2019-02-19 22:25:56 +1100

    Adding Reference
    Replace the reference to bootstrap with bootstrap-lumen in App_Start -> Bundle.config.cs

---
+++ App_Start/Bundle.config.cs
@@ -0,0 +1,6 @@
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
+                      "~/Content/site.css"));
+        }
+    }
