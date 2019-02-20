544d60c0945c6024750f964018e4e9fc2b91e57b
pulipatisatish2012 <36352775+pulipatisatish2012@users.noreply.github.com>
2019-02-20 20:48:54 +1100

    Created Movie Model

---
+++ Using ViewModel/movies.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Videly.Models
+{
+    public class Movies
+    {
+        public int mid { get; set; }
+        public string mName { get; set; }
+    }
